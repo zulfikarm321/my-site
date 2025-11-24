438d732c9f18a778b1fccc19fd6c675804136a26
Zulfikar Muhamad <starguners72@gmail.com>
2025-11-24 22:49:13 +0700

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
       <MarkdownRenderer filePath="bio.md" />
       <hr className="my-10" />
       <h2 className="mb-5">My Latest Post</h2>
-      =
+
       <PostList posts={posts} perPage={5} />
     </>
   );
